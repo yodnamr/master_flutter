03c2e74394dd267395bb56cc77d53ecfd54ce145
iBlurBlur <tanakorn.ngam@gmail.com>
2021-03-23 16:51:10 +0700

    design product list

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,22 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:my_stock/src/configs/app_route.dart';
 import 'package:my_stock/src/pages/login/background_theme.dart';
 import 'package:my_stock/src/view_models/menu_viewmodel.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,22 +12,17 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("home page"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'tanakorn',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
         ),
+        itemBuilder: (context, index) => ShopListItem(220),
+        itemCount: 100,
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          //todo
+        },
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ),
@@ -73,7 +56,7 @@ class CommonDrawer extends StatelessWidget {
               .items
               .map(
                 (e) => ListTile(
-                  onTap: (){
+                  onTap: () {
                     e.onTap(context);
                   },
                   leading: Icon(
@@ -95,6 +78,120 @@ class CommonDrawer extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
+
+
+class ShopListItem extends StatelessWidget {
+
+  final Function press;
+  final double maxHeight;
+
+  const ShopListItem(this.maxHeight, {Key key, this.press})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            _buildImage(),
+            Expanded(
+              child: _buildInfo(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Padding _buildInfo() => Padding(
+    padding: EdgeInsets.all(6),
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          'MacBook M1',
+          overflow: TextOverflow.ellipsis,
+          maxLines: 2,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              '\$ 30,000',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              '1,112 pieces',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.deepOrangeAccent,
+              ),
+            )
+          ],
+        ),
+      ],
+    ),
+  );
+
+  Stack _buildImage() {
+    final height = maxHeight - 75;
+    final productImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTwpqxUPV0asKFE9RQee4GsfjXombs6TM5tw&usqp=CAU';
+    return Stack(
+      children: [
+        productImage != null && productImage.isNotEmpty
+            ? Image.network(
+          productImage,
+          height: height,
+          width: double.infinity,
+          fit: BoxFit.cover,
+        )
+            : Image.asset(
+          'xxx',
+          height: height,
+          width: double.infinity,
+        ),
+        1 > 0
+            ? SizedBox()
+            : Positioned(
+          top: 1,
+          right: 1,
+          child: Card(
+            color: Colors.white70,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 8,
+                vertical: 4,
+              ),
+              child: Row(
+                children: <Widget>[
+                  Icon(
+                    FontAwesomeIcons.box,
+                    size: 15.0,
+                    color: Colors.black,
+                  ),
+                  SizedBox(width: 4),
+                  Text(
+                    'out of stock',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
