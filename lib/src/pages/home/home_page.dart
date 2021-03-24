476c7137044e2d72cd8ea9bc14fd46bcb9b097f0
iBlurBlur <tanakorn.ngam@gmail.com>
2021-03-24 10:24:23 +0700

    revise code

--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:my_stock/src/configs/app_route.dart';
+import 'package:my_stock/src/constants/asset.dart';
 import 'package:my_stock/src/pages/login/background_theme.dart';
 import 'package:my_stock/src/view_models/menu_viewmodel.dart';
 
@@ -15,8 +16,16 @@ class HomePage extends StatelessWidget {
       body: GridView.builder(
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
+          childAspectRatio: 0.8,
         ),
-        itemBuilder: (context, index) => ShopListItem(220),
+        itemBuilder: (context, index) => LayoutBuilder(
+          builder: (context, constraint) => ShopListItem(
+            constraint.maxHeight,
+            press: () {
+              print('click!!!');
+            },
+          ),
+        ),
         itemCount: 100,
       ),
       floatingActionButton: FloatingActionButton(
@@ -82,19 +91,16 @@ class CommonDrawer extends StatelessWidget {
   }
 }
 
-
 class ShopListItem extends StatelessWidget {
-
   final Function press;
   final double maxHeight;
 
-  const ShopListItem(this.maxHeight, {Key key, this.press})
-      : super(key: key);
+  const ShopListItem(this.maxHeight, {Key key, this.press}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: press,
+      onTap: this.press,
       child: Container(
         color: Colors.white,
         child: Column(
@@ -110,87 +116,86 @@ class ShopListItem extends StatelessWidget {
   }
 
   Padding _buildInfo() => Padding(
-    padding: EdgeInsets.all(6),
-    child: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Text(
-          'MacBook M1',
-          overflow: TextOverflow.ellipsis,
-          maxLines: 2,
-        ),
-        Row(
+        padding: EdgeInsets.all(6),
+        child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             Text(
-              '\$ 30,000',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
+              'MacBook M1',
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
             ),
-            Text(
-              '1,112 pieces',
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Colors.deepOrangeAccent,
-              ),
-            )
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  '\$ 30,000',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  '1,112 pieces',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.deepOrangeAccent,
+                  ),
+                )
+              ],
+            ),
           ],
         ),
-      ],
-    ),
-  );
+      );
 
   Stack _buildImage() {
-    final height = maxHeight - 75;
-    final productImage = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRTwpqxUPV0asKFE9RQee4GsfjXombs6TM5tw&usqp=CAU';
+    final height = maxHeight * 0.78;
+    final productImage = '';
     return Stack(
       children: [
         productImage != null && productImage.isNotEmpty
             ? Image.network(
-          productImage,
-          height: height,
-          width: double.infinity,
-          fit: BoxFit.cover,
-        )
+                productImage,
+                height: height,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              )
             : Image.asset(
-          'xxx',
-          height: height,
-          width: double.infinity,
-        ),
-        1 > 0
-            ? SizedBox()
-            : Positioned(
-          top: 1,
-          right: 1,
-          child: Card(
-            color: Colors.white70,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 8,
-                vertical: 4,
+                Asset.noPhotoImage,
+                height: height,
+                width: double.infinity,
               ),
-              child: Row(
-                children: <Widget>[
-                  Icon(
-                    FontAwesomeIcons.box,
-                    size: 15.0,
-                    color: Colors.black,
-                  ),
-                  SizedBox(width: 4),
-                  Text(
-                    'out of stock',
-                    style: TextStyle(
-                      fontSize: 12,
-                      fontWeight: FontWeight.bold,
+        if(0 <= 0)
+             Positioned(
+                top: 1,
+                right: 1,
+                child: Card(
+                  color: Colors.white70,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8,
+                      vertical: 4,
+                    ),
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.box,
+                          size: 15.0,
+                          color: Colors.black,
+                        ),
+                        SizedBox(width: 4),
+                        Text(
+                          'out of stock',
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                ],
+                ),
               ),
-            ),
-          ),
-        ),
       ],
     );
   }
