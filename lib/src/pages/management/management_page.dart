daae745f161c61bcc61f213456ba92bf1a5c46d2
iBlurBlur <tanakorn.ngam@gmail.com>
2021-03-25 14:39:44 +0700

    show modal chose camera and gallery

--- lib/src/pages/management/management_page.dart
+++ lib/src/pages/management/management_page.dart
@@ -4,6 +4,7 @@ import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:my_stock/src/constants/api.dart';
 import 'package:my_stock/src/models/product_response.dart';
 import 'package:my_stock/src/services/network.dart';
 
@@ -60,6 +61,7 @@ class _ManagementPageState extends State<ManagementPage> {
                     )
                   ],
                 ),
+                ProductImage(null, image: null)
               ],
             ),
           ),
@@ -173,3 +175,155 @@ class _ManagementPageState extends State<ManagementPage> {
     )..show(context);
   }
 }
+
+
+class ProductImage extends StatefulWidget {
+  final Function callBack;
+  final String image;
+
+  const ProductImage(this.callBack, {Key key, @required this.image})
+      : super(key: key);
+
+  @override
+  _ProductImageState createState() => _ProductImageState();
+}
+
+class _ProductImageState extends State<ProductImage> {
+  File _imageFile;
+  String _image;
+  final _picker = ImagePicker();
+
+  @override
+  void initState() {
+    _image = widget.image;
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _imageFile?.delete();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          _buildPickerImage(),
+          _buildPreviewImage(),
+        ],
+      ),
+    );
+  }
+
+  dynamic _buildPreviewImage() {
+    if ((_image == null || _image.isEmpty) && _imageFile == null) {
+      return SizedBox();
+    }
+
+    final container = (Widget child) => Container(
+      color: Colors.grey[100],
+      margin: EdgeInsets.only(top: 4),
+      alignment: Alignment.center,
+      height: 350,
+      child: child,
+    );
+
+    return _image != null
+        ? container(Image.network('${API.IMAGE_URL}/$_image'))
+        : Stack(
+      children: [
+        container(Image.file(_imageFile)),
+        _buildDeleteImageButton(),
+      ],
+    );
+  }
+
+  OutlinedButton _buildPickerImage() => OutlinedButton.icon(
+    icon: FaIcon(FontAwesomeIcons.image),
+    label: Text('image'),
+    onPressed: () {
+      _modalPickerImage();
+    },
+  );
+
+  void _modalPickerImage() {
+    final buildListTile =
+        (IconData icon, String title, ImageSource source) => ListTile(
+      leading: Icon(icon),
+      title: Text(title),
+      onTap: () {
+        Navigator.of(context).pop();
+        _pickImage(source);
+      },
+    );
+
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              buildListTile(
+                Icons.photo_camera,
+                "Take a picture from camera",
+                ImageSource.camera,
+              ),
+              buildListTile(
+                Icons.photo_library,
+                "Choose from photo library",
+                ImageSource.gallery,
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  void _pickImage(ImageSource source) {
+    _picker
+        .getImage(
+      source: source,
+      imageQuality: 70,
+      maxHeight: 500,
+      maxWidth: 500,
+    )
+        .then((file) {
+      if (file != null) {
+        setState(() {
+
+          _image = null;
+          widget.callBack(_imageFile);
+        });
+      }
+    }).catchError((error) {
+      //todo
+    });
+  }
+
+
+  Positioned _buildDeleteImageButton() => Positioned(
+    right: 0,
+    child: IconButton(
+      onPressed: () {
+        setState(() {
+          _imageFile = null;
+          widget.callBack(null);
+        });
+      },
+      icon: Icon(
+        Icons.clear,
+        color: Colors.black54,
+      ),
+      splashColor: Colors.transparent,
+      highlightColor: Colors.transparent,
+    ),
+  );
+}
+
+
