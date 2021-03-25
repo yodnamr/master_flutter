958bafc5907156f6dbc36ba7da296b82c7fd2741
iBlurBlur <tanakorn.ngam@gmail.com>
2021-03-25 16:03:10 +0700

    add google map and pin marker

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyBfv9iMFvvcNZrgu-wb5WxyghD2Nrm3xV0")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
---
+++ lib/src/pages/map/google_map_page.dart
@@ -0,0 +1,65 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+class GoogleMapPage extends StatefulWidget {
+  @override
+  State<GoogleMapPage> createState() => GoogleMapPageState();
+}
+
+class GoogleMapPageState extends State<GoogleMapPage> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  final dummyData = List<LatLng>();
+  final _marker = Set<Marker>();
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
+  @override
+  void initState() {
+    dummyData.add(LatLng(13.746774,100.5326445));
+    dummyData.add(LatLng(13.728223,100.532949));
+    dummyData.add(LatLng(13.69725,100.5131413));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        markers: _marker,
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
+    );
+  }
+
+  Future<void> _goToTheLake() async {
+    dummyData.forEach((element) {
+      final marker = Marker(
+        markerId: MarkerId(element.toString()),
+        position: element,
+      );
+      _marker.add(marker);
+    });
+
+    setState(() {});
+  }
+}
